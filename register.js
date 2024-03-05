9ed82e3f60e3e2b806460e03e14c92f2718dca17
Czbastiane <basty.ortiona@gmail.com>
2024-03-05 16:04:08 +0800

    register

---
+++ register.js
@@ -0,0 +1,27 @@
+console.log("Hello po World!");
+console.error("ERROR HAPPENED! ! !")
+console.warn("Warning!!!")
+
+//variables
+//string = "Words" , 'wowowow' , '123'
+//number = 123 , 44e, 23442.123
+//boolean = true , false
+//object = ["apple" , "banana"], [1,2,3,4]
+          // ["apple", 3, true]
+//typeof
+//console.log(typeof(["apple", "banana"]))
+
+//declaration of variables
+let name = "Czbastiane",
+ lastname = `Ortiona`,
+ age = 10,
+ isProf = true,
+ hobbies = ["Watching TV", "Playing Video Games"],
+run = function () {
+    console.log("Im running.");
+};
+console.log(name, age, isProf, hobbies)
+console.table(hobbies)
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
+         
